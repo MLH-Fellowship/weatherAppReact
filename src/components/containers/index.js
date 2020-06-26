94de199fe82ae7fc17408d5e3f15c867a5bd808c
Grimlock123 <barry.mouhamadou6@gmail.com>
2020-06-26 00:43:29 -0400

    clearing files out

--- src/components/containers/index.js
+++
@@ -1,6 +0,0 @@
-// Barrel file for container components;
-export { Login, Signup } from "./AuthFormContainer";
-export { default as AllRecipesContainer } from "./AllRecipesContainer";
-export { default as AllBookmarksContainer } from "./AllBookmarksContainer";
-export { default as AllReviewsContainer } from "./AllReviewsContainer";
-export { default as HomeContainer } from "./HomeContainer";
--- src/thunks/index.js
+++
@@ -1,5 +0,0 @@
-// Barrel file for thunks, which will be mapped to and invoked within our smart containers;
-export * from "../store/utilities/user";
-export * from "../store/utilities/allBookmarks";
-export * from "../store/utilities/allRecipes";
-export * from "../store/utilities/allReviews";
